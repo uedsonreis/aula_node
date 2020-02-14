7f1eeeb1e491991017280aa71d0a227028b98225
Uedson Reis <uedsonreis@gmail.com>
2020-02-14 09:45:44 -0300

    Primeiro commit.

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get("/teste", (req, res) => res.send("Requisição (GET) de Teste ok!"));
+
+app.listen(3000, () => console.log("App rodando na porta 3000!"));
