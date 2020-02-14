5aa05f85bbc88e91142f04225213fd45d54c9ae7
Uedson Reis <uedsonreis@gmail.com>
2020-02-14 11:29:21 -0300

    Removendo o node_modules

--- build/model/deducao.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Deducao = /** @class */ (function () {
-    function Deducao() {
-    }
-    return Deducao;
-}());
-exports.Deducao = Deducao;
--- build/model/pagador.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Pagador = /** @class */ (function () {
-    function Pagador() {
-        this.pagou = new Array();
-    }
-    return Pagador;
-}());
-exports.Pagador = Pagador;
