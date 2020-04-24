b40986ac56106d16c5fc99cd1b80a2ac3399f53a
Uedson Reis <uedsonreis@gmail.com>
2020-04-23 21:51:58 -0300

    Ajustes na aula

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import irpf from "./irpf";
 
 const app = express();
+app.use(express.json())
 app.use(cors());
 
 app.get("/teste", (req, res) => res.send("Requisição (GET) de Teste ok!"));
@@ -18,4 +19,12 @@ app.get("/irpf/:value/:name", (req, res) => {
     res.status(200).json(irpf.calculateThis(value, name));
 });
 
-app.listen(3000, () => console.log("App rodando na porta 3000!"));
+app.post("/irpf/ex2", (req, res) => {
+
+    const value = Number(req.body.value);
+    const name = req.body.name;
+
+    res.status(200).json(irpf.calculateThis(value, name));
+});
+
+app.listen(3000, () => console.log("Backend rodando na porta 3000!"));
